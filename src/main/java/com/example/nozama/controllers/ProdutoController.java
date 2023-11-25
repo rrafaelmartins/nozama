c2b7116612116b30e360f5ddd7042032cbf7e7f1
Gabriel Breder <gabrielbertobreder@gmail.com>
2023-11-25 16:49:31 -0300

    feat: "product controller"

---
+++ src/main/java/com/example/nozama/controllers/ProdutoController.java
@@ -0,0 +1,38 @@
+package com.example.nozama.controllers;
+
+import com.example.nozama.model.produto.Produto;
+import com.example.nozama.model.produto.ProdutoRequestDTO;
+import com.example.nozama.services.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+    @Autowired
+    private ProdutoService produtoService;
+
+    @GetMapping
+    public List<Produto> listarTodos() {
+        return produtoService.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Produto> obterPorId(@PathVariable Long id) {
+        return produtoService.obterPorId(id);
+    }
+
+    @PostMapping()
+    public Produto salvar(@RequestBody ProdutoRequestDTO data) {
+        Produto produto = new Produto(data);
+        return produtoService.salvar(produto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        produtoService.deletar(id);
+    }
+}
