3f51472946f00d1065ccde6b8272b26edb006204
Gabriel Breder <gabrielbertobreder@gmail.com>
2023-11-25 16:49:14 -0300

    feat: "product class"

---
+++ src/main/java/com/example/nozama/model/produto/Produto.java
@@ -0,0 +1,23 @@
+package com.example.nozama.model.produto;
+import jakarta.persistence.*;
+import lombok.*;
+@Table(name = "produto")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private Double preco;
+
+    public Produto(ProdutoRequestDTO data) {
+        this.nome = data.nome();
+        this.preco = data.preco();
+    }
+}
