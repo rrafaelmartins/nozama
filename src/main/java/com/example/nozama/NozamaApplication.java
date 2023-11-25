26faefa85020c0c585684d4bf412fad3cfbe5369
Gabriel Breder <gabrielbertobreder@gmail.com>
2023-11-25 15:56:34 -0300

    feat: "initial commit"

---
+++ src/main/java/com/example/nozama/NozamaApplication.java
@@ -0,0 +1,13 @@
+package com.example.nozama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NozamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NozamaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nozama/NozamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nozama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NozamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
