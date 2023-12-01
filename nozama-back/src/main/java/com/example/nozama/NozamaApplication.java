45c3c269933201650570ab29ca68ff0e341b4fb0
Gabriel Breder <gabrielbertobreder@gmail.com>
2023-12-01 20:30:40 -0300

    feat: implementando singleton

--- nozama-back/src/main/java/com/example/nozama/NozamaApplication.java
+++ nozama-back/src/main/java/com/example/nozama/NozamaApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NozamaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NozamaApplication.class, args);
 	}
