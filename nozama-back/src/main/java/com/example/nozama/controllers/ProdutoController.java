2b4c67880e7c4f5dd448e7735b9261bd41664aeb
Gabriel Breder <gabrielbertobreder@gmail.com>
2023-12-02 19:53:22 -0300

    merge produto controller

--- nozama-back/src/main/java/com/example/nozama/controllers/ProdutoController.java
+++ nozama-back/src/main/java/com/example/nozama/controllers/ProdutoController.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/produtos")
+@CrossOrigin("*")
 public class ProdutoController {
     @Autowired
     private ProdutoService produtoService;
